53133649e641cc0ddb27c8f07abb7c115849f219
binayakregmi <binayakforall@gmail.com>
2024-12-16 15:59:56 +0545

    first commit

---
+++ src/app/_components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h3>LOGO</h3>
+        <ul>
+            <li>
+                Home
+            </li>
+            <li>
+                About
+            </li>
+            <li>
+                Services
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/app/home/hello/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <>
+    <div>this is hello page</div>
+    <div>
+      
+    </div>
+    </>
+  )
+}
+
+export default Page
